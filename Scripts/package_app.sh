aa9d0150bcaa8dd7e9915be3f18ad100d5b01a24
Tim Yu <tim.yu@opennet.tw>
2026-08-24 21:19:41 +0800

    Show timing state on every Issue (#15)
    Co-authored-by: Tim Yu <19474225+timyeou1234@users.noreply.github.com>

--- Scripts/package_app.sh
+++ Scripts/package_app.sh
@@ -48,9 +48,9 @@ cat > "$contents/Info.plist" <<'PLIST'
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.4.8</string>
+  <string>0.4.9</string>
   <key>CFBundleVersion</key>
-  <string>12</string>
+  <string>13</string>
   <key>LSMinimumSystemVersion</key>
   <string>14.0</string>
   <key>LSUIElement</key>
